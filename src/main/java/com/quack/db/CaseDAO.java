cea54d5a208fae2536162311f25253e7efcbe58b
SirAzako <paraskosh@gmail.com>
2020-12-23 16:31:00 +0200

    Create 3 methods (Active, Recovery, Dead)

--- src/main/java/com/quack/db/CaseDAO.java
+++ src/main/java/com/quack/db/CaseDAO.java
@@ -6,43 +6,112 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 
 /**
- *
  * @author paras
  */
 public class CaseDAO {
-    
-    	public void createCase(Case krousma) throws Exception{
-            Connection con = null ;
-            String querry = "INSERT INTO Persons(CaseID, ContactsNumber, FirstName, LastName,"+
-                            "Age, PhoneNumber, Diagnosis, DimosID, Address,"+ 
-                            "StreetNumber, ZipCode) "+ " VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
 
-            try {
-                con = DB.getConnection();
-                PreparedStatement stmt = con.prepareStatement(querry);
-                stmt.setString(1, krousma.getCaseID());
-                stmt.setInt(2, krousma.getContactsNumber());
-                stmt.setString(3, krousma.getFirstName());
-                stmt.setString(4, krousma.getLastName());
-                stmt.setInt(5, krousma.getAge());
-                stmt.setString(6, krousma.getPhoneNumber());
-                stmt.setString(7, krousma.getDiagnosis());
-                stmt.setInt(8, krousma.getDimosID());
-                stmt.setString(9, krousma.getAddress());
-                stmt.setString(10, krousma.getStreetNumber());
-                stmt.setString(11, krousma.getZipCode());
-            
+    public void createCaseActive(Case krousma) throws Exception {
+        Connection con = null;
+        String querry = "INSERT INTO Persons(CaseID, ContactsNumber, FirstName, LastName," +
+                "Age, PhoneNumber, Diagnosis, DimosID, Address," +
+                "StreetNumber, ZipCode) " + " VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
 
-                stmt.executeUpdate();
-                stmt.close();
-                DB.close();
-            } catch (Exception e) {
-                throw new Exception("Error while trying to create case " + e.getMessage());
-            } finally {
-                if(con != null)
-                    con.close();
-            }
-            
-	}
+        try {
+            con = DB.getConnection();
+            PreparedStatement stmt = con.prepareStatement(querry);
+            stmt.setString(1, krousma.getCaseID());
+            stmt.setInt(2, krousma.getContactsNumber());
+            stmt.setString(3, krousma.getFirstName());
+            stmt.setString(4, krousma.getLastName());
+            stmt.setInt(5, krousma.getAge());
+            stmt.setString(6, krousma.getPhoneNumber());
+            stmt.setString(7, krousma.getDiagnosis());
+            stmt.setInt(8, krousma.getDimosID());
+            stmt.setString(9, krousma.getAddress());
+            stmt.setString(10, krousma.getStreetNumber());
+            stmt.setString(11, krousma.getZipCode());
+
+
+            stmt.executeUpdate();
+            stmt.close();
+            DB.close();
+        } catch (Exception e) {
+            throw new Exception("Error while trying to create case " + e.getMessage());
+        } finally {
+            if (con != null)
+                con.close();
+        }
+
+    }
+
+    public void createCaseRecovery(Case krousma) throws Exception {
+        Connection con = null;
+        String querry = "INSERT INTO Persons(CaseID, ContactsNumber, FirstName, LastName," +
+                "Age, PhoneNumber, Diagnosis, Recovery, DimosID, Address," +
+                "StreetNumber, ZipCode) " + " VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
+
+        try {
+            con = DB.getConnection();
+            PreparedStatement stmt = con.prepareStatement(querry);
+            stmt.setString(1, krousma.getCaseID());
+            stmt.setInt(2, krousma.getContactsNumber());
+            stmt.setString(3, krousma.getFirstName());
+            stmt.setString(4, krousma.getLastName());
+            stmt.setInt(5, krousma.getAge());
+            stmt.setString(6, krousma.getPhoneNumber());
+            stmt.setString(7, krousma.getDiagnosis());
+            stmt.setString(8, krousma.getRecovery());
+            stmt.setInt(9, krousma.getDimosID());
+            stmt.setString(10, krousma.getAddress());
+            stmt.setString(11, krousma.getStreetNumber());
+            stmt.setString(12, krousma.getZipCode());
+
+
+            stmt.executeUpdate();
+            stmt.close();
+            DB.close();
+        } catch (Exception e) {
+            throw new Exception("Error while trying to create case " + e.getMessage());
+        } finally {
+            if (con != null)
+                con.close();
+        }
+
+    }
+
+    public void createCaseDead(Case krousma) throws Exception {
+        Connection con = null;
+        String querry = "INSERT INTO Persons(CaseID, ContactsNumber, FirstName, LastName," +
+                "Age, PhoneNumber, Diagnosis, Death, DimosID, Address," +
+                "StreetNumber, ZipCode) " + " VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
+
+        try {
+            con = DB.getConnection();
+            PreparedStatement stmt = con.prepareStatement(querry);
+            stmt.setString(1, krousma.getCaseID());
+            stmt.setInt(2, krousma.getContactsNumber());
+            stmt.setString(3, krousma.getFirstName());
+            stmt.setString(4, krousma.getLastName());
+            stmt.setInt(5, krousma.getAge());
+            stmt.setString(6, krousma.getPhoneNumber());
+            stmt.setString(7, krousma.getDiagnosis());
+            stmt.setString(8, krousma.getDeath());
+            stmt.setInt(9, krousma.getDimosID());
+            stmt.setString(10, krousma.getAddress());
+            stmt.setString(11, krousma.getStreetNumber());
+            stmt.setString(12, krousma.getZipCode());
+
+
+            stmt.executeUpdate();
+            stmt.close();
+            DB.close();
+        } catch (Exception e) {
+            throw new Exception("Error while trying to create case " + e.getMessage());
+        } finally {
+            if (con != null)
+                con.close();
+        }
+
+    }
 
 }
--- src/main/java/com/quack/frontEnd/Controller.java
+++ src/main/java/com/quack/frontEnd/Controller.java
@@ -20,8 +20,54 @@ public class Controller implements Initializable {
     private boolean menuIsHide = true;
     private boolean createCaseHide = true;
 
+
     @FXML
     private AnchorPane anchorPane;
+
+    @FXML
+    private AnchorPane menuBar;
+
+    @FXML
+    private Group menuCreateButton;
+
+    @FXML
+    private AnchorPane createCasePanel;
+
+    @FXML
+    private TextField firstNameInput;
+
+    @FXML
+    private TextField lastNameInput;
+
+    @FXML
+    private TextField ageInput;
+
+    @FXML
+    private TextField phoneNumberInput;
+
+    @FXML
+    private TextField munipicipalityInput;
+
+    @FXML
+    private TextField addressInput;
+
+    @FXML
+    private TextField streetInput;
+
+    @FXML
+    private TextField zipCodeInput;
+
+    @FXML
+    private TextField contactsInput;
+
+    @FXML
+    private TextField diagnosisInput;
+
+    @FXML
+    private TextField recoveryInput;
+
+    @FXML
+    private TextField deathInput;
 
     @FXML
     private AnchorPane toolBar;
@@ -30,47 +76,131 @@ public class Controller implements Initializable {
     private ImageView menuButton;
 
     @FXML
-    private AnchorPane menuBar;
+    private Button submitCaseButton;
 
     @FXML
-    private Group create;
-
-    @FXML
-    private AnchorPane createCasePanel;
-
-
-    @FXML
-    public void menuClick(MouseEvent event) {
-        if(menuIsHide == true){
-            menuBar.setVisible(true);
-            menuIsHide = false;
-        }else{
-            menuBar.setVisible(false);
-            menuIsHide = true;
-        }
-
-
-    }
-
-    @FXML
-    public void createCaseClick(MouseEvent event) {
-        if(createCaseHide == true){
+    public void menuCreateClick(MouseEvent event) {
+        if (createCaseHide == true) {
             createCasePanel.setVisible(true);
             createCaseHide = false;
-        }else{
+        } else {
             createCasePanel.setVisible(false);
             createCaseHide = true;
         }
-
     }
 
+    @FXML
+    public void menuClick(MouseEvent event) {
+        if (menuIsHide == true) {
+            menuBar.setVisible(true);
+            menuIsHide = false;
+        } else {
+            menuBar.setVisible(false);
+            menuIsHide = true;
+        }
+    }
 
+    @FXML
+    public void submitCaseClick(MouseEvent event) {
+        String fn = null;
+        String ln = null;
+        String pn = null;
+        String ad = null;
+        String st = null;
+        String zc = null;
+        String dd = null;
+        String rd = null;
+        String dt = null;
+        int ag = 0;
+        int cn = 0;
+        int mun = 0;
+        if (!firstNameInput.getText().isEmpty()) {
+            fn = firstNameInput.getText();
+            System.out.println("Empty");
+        }
+        if (!lastNameInput.getText().isEmpty()) {
+            ln = lastNameInput.getText();
+            System.out.println("Empty");
+        }
+        if (!ageInput.getText().isEmpty()) {
+            ag = Integer.parseInt(ageInput.getText());
+            System.out.println("Empty");
+        }
+        if (!phoneNumberInput.getText().isEmpty()) {
+            pn = phoneNumberInput.getText();
+            System.out.println("Empty");
+        }
+        if (!addressInput.getText().isEmpty()) {
+            ad = addressInput.getText();
+            System.out.println("Empty");
+        }
+        if (!streetInput.getText().isEmpty()) {
+            st = streetInput.getText();
+            System.out.println("Empty");
+        }
+        if (!zipCodeInput.getText().isEmpty()) {
+            zc = zipCodeInput.getText();
+            System.out.println("Empty");
+        }
+        if (!contactsInput.getText().isEmpty()) {
+            cn = Integer.parseInt(contactsInput.getText());
+            System.out.println("Empty");
+        }
+        if (!munipicipalityInput.getText().isEmpty()) {
+            mun = Integer.parseInt(munipicipalityInput.getText());
+            System.out.println("Empty");
+        }
+        if (!diagnosisInput.getText().isEmpty()) {
+            dd = diagnosisInput.getText();
+            System.out.println("Empty");
+        }
+        if (!recoveryInput.getText().isEmpty()) {
+            rd = recoveryInput.getText();
+            System.out.println("Empty");
+        }
+        if (!deathInput.getText().isEmpty()) {
+            dt = deathInput.getText();
+            System.out.println("Empty");
+        }
 
+        if ((recoveryInput.getText().isEmpty()) && (deathInput.getText().isEmpty())) {
+            Case activeCase = new Case(dd, cn, fn, ln, ag, pn, mun, ad, st, zc);
+            CaseDAO caseDAO = new CaseDAO();
+            try {
+                caseDAO.createCaseActive(activeCase);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            System.out.println("to krousma ine energo");
+        }
+
+        if(!(recoveryInput.getText().isEmpty()) && (deathInput.getText().isEmpty())){
+            Case caseRecover = new Case(cn, dd, rd, fn, ln, ag, pn, mun, ad, st, zc);
+            CaseDAO caseDAO = new CaseDAO();
+            try {
+                caseDAO.createCaseRecovery(caseRecover);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            System.out.println("to krousma anarose!!");
+        }
+
+        if ((recoveryInput.getText().isEmpty()) && !(deathInput.getText().isEmpty())){
+            Case caseDead = new Case(dd, dt, cn, fn, ln, ag, pn, mun, ad, st, zc);
+            CaseDAO caseDAO = new CaseDAO();
+            try {
+                caseDAO.createCaseDead(caseDead);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            System.out.println("to krousma petane");
+        }
+
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
     }
-
 
 }
 
